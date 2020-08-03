51c5f661631d9e51982e1f9bc0d6bdb726d4880e
Xuanwo <github@xuanwo.io>
2020-08-03 09:47:59 +0800

    windows: Remove ole support
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- locale_windows.go
+++ locale_windows.go
@@ -3,89 +3,32 @@
 package locale
 
 import (
-	"github.com/go-ole/go-ole"
-	"github.com/go-ole/go-ole/oleutil"
 	"golang.org/x/sys/windows/registry"
 )
 
 var detectors = []detector{
-	detectViaWinRegistry,
-	detectViaWin32OLE,
+	detectViaRegistry,
 }
 
-// detectViaWin32OLE will detect system's language via w32 ole.
+// detectViaRegistry will detect language via Windows Registry
 //
-// code inspired from https://github.com/iamacarpet/go-win64api
-func detectViaWin32OLE() (langs []string, err error) {
+// ref: https://renenyffenegger.ch/notes/Windows/registry/tree/HKEY_CURRENT_USER/Control-Panel/International/index
+func detectViaRegistry() (langs []string, err error) {
 	defer func() {
-		if err == nil {
-			return
+		if err != nil {
+			err = &Error{"detect via registry", err}
 		}
-		err = &Error{"detect via win32 ole", err}
 	}()
 
-	err = ole.CoInitialize(0)
-	if err != nil {
-		return
-	}
-	defer ole.CoUninitialize()
-
-	unknown, err := oleutil.CreateObject("WbemScripting.SWbemLocator")
-	if err != nil {
-		return
-	}
-	defer unknown.Release()
-
-	wmi, err := unknown.QueryInterface(ole.IID_IDispatch)
-	if err != nil {
-		return
-	}
-	defer wmi.Release()
-
-	serviceRaw, err := oleutil.CallMethod(wmi, "ConnectServer")
-	if err != nil {
-		return
-	}
-	service := serviceRaw.ToIDispatch()
-	defer service.Release()
-
-	resultRaw, err := oleutil.CallMethod(service, "ExecQuery", "SELECT OSLanguage FROM Win32_OperatingSystem")
-	if err != nil {
-		return
-	}
-	result := resultRaw.ToIDispatch()
-	defer result.Release()
-
-	itemRaw, err := oleutil.CallMethod(result, "ItemIndex", 0)
-	if err != nil {
-		return
-	}
-	item := itemRaw.ToIDispatch()
-	defer item.Release()
-
-	languageCode, err := oleutil.GetProperty(item, "OSLanguage")
-	if err != nil {
-		return
-	}
-
-	lang, ok := osLanguageCode[uint32(languageCode.Val)]
-	if !ok {
-		err = ErrNotSupported
-		return
-	}
-	return []string{lang}, nil
-}
-
-func detectViaWinRegistry() (langs []string, err error) {
 	key, err := registry.OpenKey(registry.CURRENT_USER, `Control Panel\International`, registry.QUERY_VALUE)
 	if err != nil {
-		return nil, &Error{"opening Windows registry for reading failed", err}
+		return nil, err
 	}
 	defer key.Close()
 
 	lang, _, err := key.GetStringValue("LocaleName")
 	if err != nil {
-		return nil, &Error{"reading a locale name from registry failed", err}
+		return nil, err
 	}
 
 	return []string{lang}, nil
