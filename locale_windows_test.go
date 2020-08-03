345a246726a0f20a8e2e6eaba4453466af0e6c4e
Xuanwo <github@xuanwo.io>
2020-08-03 09:53:23 +0800

    windows: Fix tests
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- locale_windows_test.go
+++ locale_windows_test.go
@@ -8,9 +8,9 @@ import (
 	. "github.com/smartystreets/goconvey/convey"
 )
 
-func Test_detectViaWinRegistry(t *testing.T) {
+func Test_detectViaRegistry(t *testing.T) {
 	Convey("detect via Windows Registry", t, func() {
-		langs, err := detectViaWinRegistry()
+		langs, err := detectViaRegistry()
 
 		Convey("The error should not be nil", func() {
 			So(err, ShouldBeNil)
